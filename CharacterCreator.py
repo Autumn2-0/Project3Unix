ccaf069894d8c65347809844875a918be2db67e9
Jessica Orefice <J1422666@vt.edu>
2020-12-01 22:57:55 -0500

    Created main files for character creator with comments.

---
+++ CharacterCreator.py
@@ -0,0 +1,12 @@
+#character creator class
+
+
+#implements the character creator class
+
+
+#character class - (includes but not limited to)
+#	create - creates character
+#	newCharacteroptions - after new character created
+#	modify - modify character file
+#	deletecharacter - delete a character
+#	listallCharacters - lists all character files
---
+++ main.py
@@ -0,0 +1,14 @@
+#Character creator main file
+
+#runs character creator
+
+
+#prints message
+#   Please choose a command:
+#	1. Create a character
+#	2. Modify a character
+#	3. Delete a character
+#	4. List characters
+#	5. Exit
+
+#main function with selections
