a54b2e4bed9ade89cc2626910438fd990e23c152
Jessica Orefice <J1422666@vt.edu>
2020-12-02 02:31:27 -0500

    Worked on listing out options.

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -1,7 +1,8 @@
-#character creator class
-
+#!/usr/bin/env python
 import os
 import shutil
+
+#character creator class
 #implements the character creator class
 
 
@@ -26,8 +27,6 @@ import shutil
 
 # print os.getcwd() - current work directory
 # os.system() - use a shell command move to correct dir?
-
-
 
 #create character
 def create():
@@ -69,34 +68,88 @@ def modify():
 		#updates as it goes
 	
 		#once end of file reached, say so
-		print 'End of the file reached'
+		print('End of the file reached')
 		
 		#close file
 		
 	else:
-		print 'Error: File not found', filename
-	
+		print('Error: File not found', filename)
+
+#delete character
 def deletecharacter():
 	#ask for character name
-	firstname = raw_input('Characters first name:')
+	firstname = input('Characters first name:')
 	filename = firstname + ".txt";
 	
 	#searches for file
 	if os.path.exists(filename):
 	
 		#asks if user is sure
-		print 'Press Y if you would like to delete the character', firstname
-		correct = raw_input()
+		print('Press Y if you would like to delete the character', firstname)
+		correct = input()
 
 		#If Y pressed, delete file (UNIX delete)
-		if correct == "Y":
+		if correct == "Y" or correct == "y":
 			#delete
 			os.remove(filename)
 		
 	else:
-		print 'Error: File not found', filename
-
+		print('Error: File', filename, 'not found')
 	
 def listallCharacters():
 	#lists out all file names
-	#os.listdir(mainpath)
+	directory = os.getcwd()
+	print(os.listdir(directory))
+	
+#prints the options
+def printOptions():
+
+	print('Please choose a command:')
+	print(' ')
+	print('1. Create a character')
+	print('2. Modify a character')
+	print('3. Delete a character')
+	print('4. List characters')
+	print('5. Exit')
+	print(' ')
+	print('Enter your selection:')
+	
+#determines which option was selected
+def switch_demo(selection):
+		switcher = {
+			1: "Create",
+			2: "Modify",
+            3: "Delete",
+            4: "List",
+            5: "Thank you for using the creator!"
+			}
+		return(switcher.get(selection, "Invalid option!"))
+
+
+
+#start of the main function
+
+selection = int(0)
+
+print('Welcome to the Character Creator!')
+
+while (selection != 5):
+	printOptions() #Displays options
+	selection = int(input())
+	choice = switch_demo(selection)
+	
+	if(choice == "Create"):
+		create()
+		
+	elif(choice == "Modify"):
+		modify()
+		
+	elif(choice == "Delete"):
+		deletecharacter()
+		
+	elif(choice == "List"):
+		listallCharacters()
+		
+	else:
+		print(choice)
+		
