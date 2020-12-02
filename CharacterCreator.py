a163768c9b4d0167bef03518dea9e44add7f476b
Jessica Orefice <J1422666@vt.edu>
2020-12-01 23:24:41 -0500

    Added create function

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -10,3 +10,24 @@
 #	modify - modify character file
 #	deletecharacter - delete a character
 #	listallCharacters - lists all character files
+
+#class CharacterCreator:
+
+#create character
+def create():
+	#ask for character name
+	firstname = raw_input('Characters first name:')
+	
+	#confirm the name
+	print 'Type Y to confirm the name is', firstname
+	correct = raw_input()
+	
+	if correct == "Y":
+		filename = firstname + ".txt"
+		
+		#check if there already is a file with that name?
+		#copies base file and renames with character name
+		#could use UNIX commands here (find, copy, rename)
+	
+	else:
+		create( )
