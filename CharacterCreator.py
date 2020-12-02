73b3647b05c533b03f45e4330d3ff2d31414c645
Jessica Orefice <J1422666@vt.edu>
2020-12-01 23:35:03 -0500

    Added defintions for modify, delete, and list

--- CharacterCreator.py
+++ CharacterCreator.py
@@ -28,6 +28,45 @@ def create():
 		#check if there already is a file with that name?
 		#copies base file and renames with character name
 		#could use UNIX commands here (find, copy, rename)
-	
+		
 	else:
 		create( )
+		
+def modify():
+	#ask for character name
+	firstname = raw_input('Characters first name:')
+	filename = firstname + ".txt";
+	
+	#finds file (use UNIX find)
+	#if file not found, print message saying say so
+	
+	#opens file if file exists
+	charfile = open(filename, 'w') 
+	
+	#reads off lines with [#]
+	#lets user type in or skip
+	#updates as it goes
+	
+	#once end of file reached, say so 
+	#close file
+	
+def deletecharacter():
+	#ask for character name
+	firstname = raw_input('Characters first name:')
+	filename = firstname + ".txt";
+	
+	#finds file (use UNIX find)
+	#if file not found, print message saying say so
+	
+	#asks if user is sure
+	print 'Press Y if you would like to delete the character', firstname
+	correct = raw_input()
+
+	#If Y pressed, delete file (UNIX delete)
+	if correct == "Y":
+		#delete
+
+	
+def listallCharacters():
+	#lists out all file names
+	#find all files in folder and print out names
